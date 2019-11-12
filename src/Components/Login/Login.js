552ed51199594b0905b7e67aabddb64404ace519
KandareJ <jace.kandare@yahoo.com>
2019-11-12 15:51:56 -0700

    combined code

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -8,11 +8,11 @@ class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      host: "localhost",
-      port: "8080",
-      eci: "WF7oxKhovWvcXR2Jg7mTaK",
-      protocol: "http",
-      rid: "pico_app"
+      host: "",
+      port: "",
+      eci: "",
+      protocol: "",
+      rid: ""
     }
 
     this.onChange = this.onChange.bind(this);
